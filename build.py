f3d5558450d22b382337b787668e0cae16b95c5c
Caali <modsfortera@gmail.com>
2018-12-14 23:14:52 +0100

    Check for duplicate names and fix module list.

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import json
 # Download all module.json files from the list
 lines = open('modulelist.txt').readlines()
 result = []
+names = set()
 for url in lines:
   try:
     myreq = urllib.request.urlopen(url)
@@ -13,7 +14,10 @@ for url in lines:
     module = json.loads(mydata.decode('utf-8'))
     if 'name' not in module:
         print('No name specified:', url)
+    elif module['name'].lower() in names:
+        print('Duplicate module name:', url)
     else:
+        names.add(module['name'].lower())
         result.append(module)
   except:
     print(url)
