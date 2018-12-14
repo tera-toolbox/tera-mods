63ad7cba94a1c10e225a6bee57546d7c58cc6e26
Caali <modsfortera@gmail.com>
2018-12-14 02:08:46 +0100

    Initial commit.

---
+++ build.py
@@ -0,0 +1,26 @@
+import urllib.request
+import os
+import json
+
+# Download all module.json files from the list
+lines = open('modulelist.txt').readlines()
+result = []
+for url in lines:
+  try:
+    myreq = urllib.request.urlopen(url)
+    mydata = myreq.read()
+    
+    module = json.loads(mydata.decode('utf-8'))
+    if 'name' not in module:
+        print('No name specified:', url)
+    else:
+        result.append(module)
+  except:
+    print(url)
+
+# Sort by name
+result = sorted(result, key = lambda x: x['name'])
+
+with open('modulelist.json', 'w') as fh:
+    json.dump(result, fh)
+    
