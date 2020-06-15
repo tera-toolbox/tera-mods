bd8d557bacf974ed2b72890bdfa94bab9d02110b
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-06-15 23:25:40 +0300

    update list

--- build.py
+++ build.py
@@ -30,10 +30,28 @@ for url in lines:
     module = json.loads(mydata.decode('utf-8'))
     if 'name' not in module:
         print('No name specified:', url)
-    elif module['name'].lower() in names:
-        print('Duplicate module name:', url)
     else:
+        if module['name'].lower() in names:
+            print('WARNING: Duplicate module name:', url)
+
         names.add(module['name'].lower())
+
+        # Upgrade legacy fields
+        if 'options' in module:
+            if 'niceName' in module['options']:
+                module['options']['cliName'] = module['options']['niceName']
+                del module['options']['niceName']
+
+        if 'category' in module:
+            if 'keywords' not in module:
+                module['keywords'] = [module['category']]
+            else:
+                module['keywords'].append(module['category'])
+            del module['category']
+        else:
+            if 'keywords' not in module:
+                module['keywords'] = ['network']
+
         result.append(module)
   except:
     print(url)
@@ -43,8 +61,8 @@ def guiname(x):
     if 'options' in x:
         if 'guiName' in x['options']:
             return strip_tags(x['options']['guiName'])
-        if 'niceName' in x['options']:
-            return strip_tags(x['options']['niceName'])
+        if 'cliName' in x['options']:
+            return strip_tags(x['options']['cliName'])
     return x['name']
     
 result = sorted(result, key = lambda x: guiname(x).lower())
@@ -53,5 +71,5 @@ with open('modulelist.json', 'w') as fh:
     json.dump(result, fh)
 
 print(len(result), 'mods listed!')
-print(sum(1 for x in result if 'category' not in x or x['category'] != 'client'), 'network mods listed!')
-print(sum(1 for x in result if 'category' in x and x['category'] == 'client'), 'client mods listed!')
+print(sum(1 for x in result if 'network' in x['keywords']), 'network mods listed!')
+print(sum(1 for x in result if 'client' in x['keywords']), 'client mods listed!')
